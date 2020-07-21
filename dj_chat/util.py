b597ef8095467438dca2ca436c3e7ac28f62da3d
chao <408737515@qq.com>
2020-07-21 19:53:29 +0800

    显示bug

--- dj_chat/util.py
+++ dj_chat/util.py
@@ -13,12 +13,17 @@ class ChatCache(object):
         self.chat_cache[chat_group].add(profile_uid)
         cache.set('chats', self.chat_cache)
 
+    def append_delay(self, chat_group, profile_uid, delay=10):
+        print('add', self.chat_cache[chat_group])
+        self.chat_cache[chat_group].add(profile_uid)
+        cache.set('chats', self.chat_cache, delay)
+
     def remove(self, chat_group, profile_uid):
         self.chat_cache[chat_group].remove(profile_uid)
         cache.set('chats', self.chat_cache)
 
     def get_cache(self, chat_group):
-        return self.chat_cache[chat_group]
+        return cache.get('chats', defaultdict(set)).get(chat_group, defaultdict(set))
 
 
 if __name__ == '__main__':
@@ -27,9 +32,13 @@ if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_chat.settings")
 
     ca = ChatCache()
-    ca.append('test', 'ca-123')
-    ca.append('test', 'ca-124')
     print(ca.get_cache('test'))
-    ca.remove('test', 'ca-123')
+
+    # ca.append('test', 'ca-123')
+    ca.append_delay('test', 'ca-124', 2)
     print(ca.get_cache('test'))
+    import time
+
+    time.sleep(3)
     print(ca.get_cache('test'))
+    print(ca.get_cache('chats'))
